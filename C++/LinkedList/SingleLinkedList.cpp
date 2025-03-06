45ee785e4e0dbd8bbec47e580fa1b2ec80df7148
Mohamed Abd El-Kareem <130367629+Mohamd-Abdelkreem@users.noreply.github.com>
2025-03-06 02:43:21 +0200

    Update SingleLinkedList.cpp

--- C++/LinkedList/SingleLinkedList.cpp
+++ C++/LinkedList/SingleLinkedList.cpp
@@ -6,20 +6,20 @@ class Node
 {
 public:
     int Value;
-    Node *Next;
+    Node* Next;
 };
 class LinkedList
 {
 
 public:
-    void PushFront(Node *&Head, int value)
+    void PushFront(Node*& Head, int value)
     {
-        Node *newNode = new Node();
+        Node* newNode = new Node();
         newNode->Value = value;
         newNode->Next = Head;
         Head = newNode;
     }
-    void PrintList(Node *Head)
+    void PrintList(Node* Head)
     {
         while (Head != NULL)
         {
@@ -27,7 +27,7 @@ public:
             Head = Head->Next;
         }
     }
-    Node *Find(Node *Head, int node)
+    Node* Find(Node* Head, int node)
     {
         while (Head != NULL)
         {
@@ -38,25 +38,25 @@ public:
         return NULL;
     }
 
-    Node *PushAfter(Node *Head, int node, int value)
+    void PushAfter(Node* Head, int node, int value)
     {
-        Node *IsNodeExist = Find(Head, node);
+        Node* IsNodeExist = Find(Head, node);
         if (IsNodeExist != NULL)
         {
-            Node *newNode = new Node;
+            Node* newNode = new Node;
             newNode->Next = IsNodeExist->Next;
             newNode->Value = value;
             IsNodeExist->Next = newNode;
         }
         else
         {
-            return NULL;
+            return ;
         }
     }
-    void PushBack(Node *&Head, int value)
+    void PushBack(Node*& Head, int value)
     {
-        Node *LastNode = new Node;
-        Node *newNode = new Node;
+        Node* LastNode = new Node;
+        Node* newNode = new Node;
         newNode->Next = NULL;
         newNode->Value = value;
         if (Head == NULL)
@@ -73,10 +73,10 @@ public:
             LastNode->Next = newNode;
         }
     }
-    void DeleteNode(Node *&Head, int node)
+    void DeleteNode(Node*& Head, int node)
     {
-        Node *CurrNode = new Node;
-        Node *PrevNode = new Node;
+        Node* CurrNode = new Node;
+        Node* PrevNode = new Node;
         CurrNode = Head;
         PrevNode = Head;
         if (Head == NULL)
@@ -100,9 +100,9 @@ public:
         PrevNode->Next = CurrNode->Next;
         delete CurrNode;
     }
-    void DeleteFirstNode(Node *&Head)
+    void DeleteFirstNode(Node*& Head)
     {
-        Node *Current = Head;
+        Node* Current = Head;
         if (Current == NULL)
         {
             return;
@@ -110,10 +110,10 @@ public:
         Head = Current->Next;
         delete Current;
     }
-    void DeletLastNode(Node *&Head)
+    void DeletLastNode(Node*& Head)
     {
-        Node *PrevNode = Head;
-        Node *CurrNode = Head;
+        Node* PrevNode = Head;
+        Node* CurrNode = Head;
         if (CurrNode == NULL)
             return;
         if (CurrNode->Next == NULL)
@@ -135,15 +135,14 @@ public:
 
 int main()
 {
-    Node *Head = NULL;
+    Node* Head = NULL;
 
     LinkedList l1;
     l1.PushFront(Head, 3);
 
     l1.PushFront(Head, 2);
     l1.PushFront(Head, 1);
-
-    l1.DeletLastNode(Head);
+    l1.PushAfter(Head, 3,5);
 
     l1.PrintList(Head);
 }
